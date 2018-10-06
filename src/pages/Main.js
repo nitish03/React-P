092b7f52581b86623d013a4f3a6873de1db4aa53
Nitish Ranjan <nitish.ranjan03@gmail.com>
2018-10-06 15:29:24 -0400

    moved search page codes in Main and  Search pages, imported it & Routed the paths

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import BookShelf from '../components/BookShelf'
+import More from '../components/More'
 
 class Main extends Component {
   render() {
     return(
-
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+        <BookShelf/>
+        </div>
+        <More/>
+      </div>
     );
   }
 }
